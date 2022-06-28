7d1f399bad0c7f6e77f644f3ee6e03297dc73a01
mloser <zh975525512@gmail.com>
2022-06-28 12:10:11 +0800

    修改线程池中线程删除的逻辑

--- BluePoints/image_transfer.py
+++ BluePoints/image_transfer.py
@@ -1,14 +1,19 @@
+import os
+import time
+
 from flask import Blueprint, request, jsonify
 from config import *
 from concurrent.futures import ThreadPoolExecutor
 from server.tool import *
 import datetime
 import random
+from threading import Thread
 
 
 bp = Blueprint('image_transfer', __name__, url_prefix='/api/image')
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'JPG', 'PNG'}
-executor = ThreadPoolExecutor()
+executor = ThreadPoolExecutor(2)
+thread_maps = dict()
 
 
 # 判断是否是允许的文件类型
@@ -30,6 +35,27 @@ def generate_image_name():
     return now_time + rand + '.png'
 
 
+# 线程池中线程的回调函数
+def call_back(feature):
+    filename = [k for k, v in thread_maps.items() if v == feature][0]
+    print('==================' + str(filename) + '-----------------------------------------------')
+    if os.path.exists(os.path.join(image_upload_path, str(filename))):
+        os.remove(os.path.join(image_upload_path, str(filename)))
+        print('--------------------remove upload----------------------')
+    if os.path.exists(os.path.join(image_mask_path, str(filename))):
+        os.remove(os.path.join(image_mask_path, str(filename)))
+        print('--------------------remove mask-------------------')
+
+
+# 删除已处理的图片的线程目标
+def delete_download(filename):
+    while True:
+        if os.path.exists(os.path.join(image_download_path, filename)):
+            os.remove(os.path.join(image_download_path, filename))
+            return
+        time.sleep(5)
+
+
 @bp.route('/identification', methods=['POST'])
 def get_id_image():
     try:
@@ -37,7 +63,10 @@ def get_id_image():
         new_filename = generate_image_name()
         image_path = os.path.join(image_upload_path, new_filename)
         img.save(image_path)
-        executor.submit(get_identification_image, image_path, image_mask_path, image_download_path)
+        feature = executor.submit(get_identification_image, image_path, image_mask_path, image_download_path)
+        feature.add_done_callback(call_back)
+        thread_maps.update({new_filename: feature})
+        print('----------------------------add maps---------------------')
         return jsonify({'status': 'success', 'message': new_filename})
     except Exception as err:
         return jsonify({'status': 'failed', 'message': str(err)})
@@ -50,7 +79,10 @@ def get_segmentation_image():
         new_filename = generate_image_name()
         image_path = os.path.join(image_upload_path, new_filename)
         img.save(image_path)
-        executor.submit(img_matting, image_path, image_mask_path, image_download_path)
+        feature = executor.submit(img_matting, image_path, image_mask_path, image_download_path)
+        feature.add_done_callback(call_back)
+        thread_maps.update({new_filename: feature})
+        print('----------------------------add maps---------------------')
         return jsonify({'status': 'success', 'message': new_filename})
     except Exception as err:
         return jsonify({'status': 'failed', 'message': str(err)})
@@ -74,13 +106,14 @@ def download_image():
 @bp.route('/delete', methods=['POST'])
 def delete_image():
     filename = request.get_json().get('filename')
+    feature = thread_maps.get(filename)
     try:
-        if os.path.exists(os.path.join(image_upload_path, filename)):
+        if feature.cancel():
             os.remove(os.path.join(image_upload_path, filename))
-        if os.path.exists(os.path.join(image_mask_path, filename)):
-            os.remove(os.path.join(image_mask_path, filename))
-        if os.path.exists(os.path.join(image_download_path, filename)):
-            os.remove(os.path.join(image_download_path, filename))
+        else:
+            Thread(target=delete_download, args=filename).start()
+
+        thread_maps.pop(filename)
         return jsonify({'status': 'success'})
     except Exception as err:
         return jsonify({'status': 'failed', 'message': str(err)})
