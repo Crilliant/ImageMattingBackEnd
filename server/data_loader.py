0ac54c90c51a64d71d97d0fa947109e151f2f5a2
mloser <zh975525512@gmail.com>
2022-07-02 09:01:57 +0800

    temp

--- server/data_loader.py
+++ server/data_loader.py
@@ -253,24 +253,24 @@ class SalObjDataset(Dataset):
         # image = Image.open(self.image_name_list[idx])#io.imread(self.image_name_list[idx])
         # label = Image.open(self.label_name_list[idx])#io.imread(self.label_name_list[idx])
 
-        image = io.imread(self.image_name_list[idx])
+        image = Image.open(self.image_name_list[idx])
         imname = self.image_name_list[idx]
         imidx = np.array([idx])
 
-        if (0 == len(self.label_name_list)):
+        if 0 == len(self.label_name_list):
             label_3 = np.zeros(image.shape)
         else:
             label_3 = io.imread(self.label_name_list[idx])
 
         label = np.zeros(label_3.shape[0:2])
-        if (3 == len(label_3.shape)):
+        if 3 == len(label_3.shape):
             label = label_3[:, :, 0]
-        elif (2 == len(label_3.shape)):
+        elif 2 == len(label_3.shape):
             label = label_3
 
-        if (3 == len(image.shape) and 2 == len(label.shape)):
+        if 3 == len(image.shape) and 2 == len(label.shape):
             label = label[:, :, np.newaxis]
-        elif (2 == len(image.shape) and 2 == len(label.shape)):
+        elif 2 == len(image.shape) and 2 == len(label.shape):
             image = image[:, :, np.newaxis]
             label = label[:, :, np.newaxis]
 
